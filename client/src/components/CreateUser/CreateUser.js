56e8d9fd7da9eb8a171c789602793df3b16f3df6
hartmanningvarsson <svuntutussa@gmail.com>
2019-03-20 13:11:11 +0000

    Harti

--- client/src/components/CreateUser/CreateUser.js
+++ client/src/components/CreateUser/CreateUser.js
@@ -3,70 +3,53 @@ import SocketContext from '../../contexts/SocketContext';
 
 /* CreateUser og constructor alveg eins og chatwindow */
 class CreateUser extends React.Component {
-componentDidCatch() {
+componentDidMatch() {
   const { socket } = this.context;
-<<<<<<< HEAD
-  socket.on('adduser', username => {
-    const { username } = this.state;
-    this.setState({ username});
-=======
-  socket.on('userName', userName => {
-    const { userNames } = this.state;
-    this.setState({ userNames: [ ...userNames, userName]});
->>>>>>> 25519482e51d2da4aae0be9f964ffdfb94fd04ea
+  socket.on('adduser', users => {
+      console.log(users);
+      this._populateUserList(users);
+  });
+}
+
+_populateUserList(users){
+  this.setState({
+    users: users.map((u, idx) => `User ${idx + 1}`)
   });
 }
 
 constructor(props) {
   super(props);
   this.state ={
-    userNames: [], /*All the usernames currently taken */
+    users: [], /*All the usernames currently taken */
     userName: '' /* current username */
   }
 }
 
+
+
 render() {
+  
+  const { userName } = this.state;
   return (
     <div className="name-window">
       <div className="input-container">
-        <input type="text" value={ username } onChange={e => this.setState({ username: e.target.value })} placeholder="Enter your username here..." />
-        <button type="button" onClick={() => this.handleRegister(username)}>Ok!</button>
+        <input type="text" value={ userName } onChange={e => this.setState({ userName: e.target.value })} placeholder="Enter your username here..." />
+        <button type="button" onClick={() => this.handleRegister(userName)}>Ok!</button>
       </div>
     </div>
   )
 }
 /*tók callback sem parameter, óþarfi? */
 handleRegister(userName) {
-  if (!isUsernameAvailable(userName)) {
-    return callback('user is not available');
-  }
-<<<<<<< HEAD
-  else(componentDidCatch);
-  this.setState({ username: e.target.value});
-=======
-  else {
-    userNames.push(userName);
-    componentDidCatch();
-  }
->>>>>>> 25519482e51d2da4aae0be9f964ffdfb94fd04ea
+  const { socket } = this.context;
+  socket.emit('adduser', this.userName);
+  this.setState({ userName: this.userName });
+  
 }
 
-}
-    
 
 
-/*IsUsernameAvailable function returns boolean */
-isUsernameAvailable(userName){
-  var length = usernames.length;
-  for (var i = 0; i < length; i++) {
-    if(userName == usernames[i]) {
-      return false;
-    }
-    else {
-      return true;
-    }
-  }
-}
+};
 
 CreateUser.contextType = SocketContext;
 
